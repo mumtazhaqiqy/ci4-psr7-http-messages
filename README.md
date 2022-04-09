14396080f44d563d5aaf349c6cdc84d7a08a5d81
mumtazhaqiqy <mumtazhaqiqy@gmail.com>
2022-04-09 22:24:34 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ci4-psr7-http-messages
+# CI4 PSR-7 HTTP Messages
+
+This package converst codeigniter4-http object from and to objects implementing HTTP message interfaces defined by PSR-7.
+
+## Instalation
+
+---sh
+$ composer require mumtazhaqiqy/ci4-psr7-http-messages
+
+$ composer require nyholm/psr7
+---
+
+## Usage
+
+The bridge provides an interface of a factory called
+``CodeIgniter\Psr7Bridge\Interfaces\HttpPsr7FactoryInterface``
+that builds objects implementing PSR-7 interfaces from ``IncommingRequest`` objects.
+
+The following code snippet explains how to convert a ``CodeIgniter\HTTP\IncomingRequest``
+to a ``Nyholm\Psr7\ServerRequest`` class implementing the
+``Psr\Http\Message\ServerRequestInterface`` interface:
+
+```php
+<?php
+
+use CodeIgniter\Config\Services;
+use CodeIgniter\Psr7Bridge\HttpPsr7Factory;
+use Nyholm\Psr7\Factory\Psr17Factory;
+
+$requestCodeIgniter = Services::request();
+
+$psr17Factory = new Psr17Factory();
+$psrHttpFactory = new HttpPsr7Factory($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+$psrRequest = $psrHttpFactory->createRequest($requestCodeIgniter);
+```
---
+++ test/HttpPsr7FactoryTest.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace mumtazhaqiqy\Codeigniter4Psr7\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+*  Corresponding Class to test YourClass class
+*
+*  For each class in your library, there should be a corresponding Unit-Test for it
+*  Unit-Tests should be as much as possible independent from other test going on.
+*
+*  @author yourname
+*/
+class YourClassTest extends TestCase
+{
+	
+  /**
+  * Just check if the YourClass has no syntax error 
+  *
+  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+  * any typo before you even use this library in a real project.
+  *
+  */
+  public function testIsThereAnySyntaxError()
+  {
+	$var = new mumtazhaqiqy\Codeigniter4Psr7\HttpPsr7Factory;
+	$this->assertTrue(is_object($var));
+	unset($var);
+  }
+  
+  /**
+  * Just check if the YourClass has no syntax error 
+  *
+  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+  * any typo before you even use this library in a real project.
+  *
+  */
+  public function testMethod1()
+  {
+	$var = new mumtazhaqiqy\Codeigniter4Psr7\HttpPsr7Factory;
+	$this->assertTrue($var->method1("hey") == 'Hello World');
+	unset($var);
+  } 
+}
